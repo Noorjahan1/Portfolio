903def68f32aa2ce3bc19b3408375ebe6e4d9cb4
Noorjahan1 <sumaiya.noorjahan@yahoo.com>
2021-10-20 19:27:23 +0600

    first commit frontend

---
+++ generateform.php
@@ -0,0 +1,10 @@
+<?php
+ 
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+  $type = test_input($_POST["name"]);
+  $email = test_input($_POST["email"]);
+  $website = test_input($_POST["website"]);
+  $comment = test_input($_POST["comment"]);
+  $gender = test_input($_POST["gender"]);
+}
+?>
